626bc30756b9f53e80401ab6ac517cf15c542cda
max <mpano91@gmail.com>
2020-09-30 17:13:45 +0300

    setup project

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+
+
+int main(int argc, char** argv){
+  std::cout<<"Hello cv" <<"\n";
+  return 0;
+}
